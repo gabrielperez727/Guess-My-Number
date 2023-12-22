62e0f242c92039fa704a7d6d4d60b27bcd48ac8a
Gabriel Perez <gabrielrperez.dev@gmail.com>
2023-12-22 04:38:53 -0500

    added random number generator

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+let number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = number;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   if (!guess) {
     document.querySelector(".message").textContent = "No number";
   }
 });
+
+console.log(number);
