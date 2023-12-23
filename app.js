5a80d20d35ae4db4feece3b27f478ba76cfdfb05
Gabriel Perez <gabrielrperez.dev@gmail.com>
2023-12-23 03:48:12 -0500

    added high score functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let number = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
+let highScore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -11,6 +12,10 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".number").textContent = number;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
   } else if (guess > number) {
     if (score > 1) {
       document.querySelector(".message").textContent = "Too high!";
